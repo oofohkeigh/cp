8140f67d549f4ba7f9438ef3bbd63c653822f234
ayuushhh <71841094+ayuushhh@users.noreply.github.com>
2023-10-06 20:27:45 +0530

    Closes #31

--- codechef/possible_victory.cpp
+++ codechef/possible_victory.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main(){
     int r, o, c;
     cin>>r>>o>>c;
-
-    if(r-c == (20-o)*36)   cout<<"NO";
-    else if(r-c < (20-o)*36)   cout<<"YES";
-    else    cout<<"NO";
- cout << endl;
+cin>>r>>o>>c;
+ //    if(r-c == (20-o)*36)   cout<<"NO";
+ //    else if(r-c < (20-o)*36)   cout<<"YES";
+ //    else    cout<<"NO";
+ // cout << endl;
     return 0;
 }
